220bdd57b5c236aaad93dd461b6bc4fae08f1203
Colin Fletcher <colin.fletcher@fiserv.com>
2020-02-16 11:36:37 -0500

    Node and LL impl I

---
+++ com/colinf/code/src/Node.java
@@ -0,0 +1,32 @@
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
+
+public class LinkedList {
+    Node head;
+
+    public void append(int data) {
+        if (head == null) {
+            head = new Node(data);
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next; //get to end
+        }
+        current.next = new Node(data);
+    }
+    
+    public void prepend(int data) {
+        Node newHead = new Node(data);
+        newHead.next = head;
+        head = newHead;
+        return;
+    }
+
+}
