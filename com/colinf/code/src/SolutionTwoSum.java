2daeac2add0d6a4f7613f4c00fc016429bf9c71f
Colin Fletcher <colin.fletcher@fiserv.com>
2020-03-05 17:41:52 -0500

    Two different Two Sum Methods

--- com/colinf/code/src/SolutionTwoSum.java
+++ com/colinf/code/src/SolutionTwoSum.java
@@ -1,20 +1,30 @@
 package com.colinf.code.src;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class SolutionTwoSum {
 
     public int[] twoSumSorted(int[] nums, int target) {
-
         // use two pointers 
+        nums = sort(nums);
         int pointerOne = 0;
         int pointerTwo = nums.length -1 ; 
-        
-        // traverse based on sum vs target
+
+        // traverse based on sum vs target;
+        // return the location of 
         while (pointerOne < pointerTwo) {
             int sum = nums[pointerOne]+nums[pointerTwo];
-            if (sum == target) {return new  int []{nums[pointerOne], nums[pointerTwo]}; }
+            if (sum == target) {return new  int []{pointerOne, pointerTwo}; }
             if (sum < target) pointerOne++; 
             else if (sum > target) pointerTwo--;
         }
         return nums;
+    }
+
+    public int[] sort(int[] vals) {
+
+         Arrays.sort(vals);
+         return vals;
     }
 }
